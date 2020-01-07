4d96aed8334bb9e22c90bb3ea10f64f7078e9a06
杨超 <yangchao@superys-MacBook-Pro.local>
2020-01-07 10:14:32 +0800

    update

--- src/algorithm/201912/myPow.py
+++ src/algorithm/201912/myPow.py
@@ -27,8 +27,31 @@ def myPow(x: float, n: int):
             s = s * x
         return 1 / s
 
+def myPow1(x: float, n: int):
+    """
+    递归计算(二分法)
+    when x == 0 or x == 1，return x
+    when n == 0，return 1
+    when n < 0，return 1 / myPow1(x, -(n + 1)) / x
+    when n % 2 == 1 return myPow1(x * x, n / 2) * x
+    when n % 2 == 0 return myPow1(x * x, n /2)
+    :param x:
+    :param n:
+    :return:
+    """
+    if x == 0 or x == 1:
+        return x
+    elif n == 0:
+        return 1
+    elif n < 0:
+        return 1 / myPow1(x, -(n + 1)) / x
+    elif n % 2 == 1:
+        return myPow1(x * x, n // 2) * x
+    elif n % 2 == 0:
+        return myPow1(x * x, n // 2)
+
 
 if __name__ == '__main__':
-    print(myPow(2, 5))
+    print(myPow1(2, 5))
     # for i in range(2, 10, 2):
     #     print(i)
